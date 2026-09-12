cdba5f1f58ad935f64fe0f0532c58d67f4bd1a02
Chris <morenochristopher580@gmail.com>
2026-09-12 15:42:09 -0500

    Create protocol.js

---
+++ shared/protocol.js
@@ -0,0 +1,36 @@
+export const C2S = {
+  JOIN: 'join',
+  INPUT: 'input',
+  SHOOT: 'shoot',
+  VOTE: 'vote',
+  RESPAWN: 'respawn',
+  SET_NAME: 'setName',
+};
+
+export const S2C = {
+  WELCOME: 'welcome',
+  PLAYER_JOINED: 'playerJoined',
+  PLAYER_LEFT: 'playerLeft',
+  SNAPSHOT: 'snapshot',
+  SHOT: 'shot',
+  DAMAGE: 'damage',
+  DEATH: 'death',
+  RESPAWN: 'respawn',
+  MATCH_STATE: 'matchState',
+  KILLFEED: 'killfeed',
+};
+
+export function publicPlayer(p) {
+  return {
+    id: p.id,
+    name: p.name,
+    team: p.team,
+    x: p.x, y: p.y, z: p.z,
+    yaw: p.yaw,
+    pitch: p.pitch,
+    health: p.health,
+    alive: p.alive,
+    kills: p.kills,
+    deaths: p.deaths,
+  };
+}
