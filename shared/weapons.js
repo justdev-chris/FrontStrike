f1b62cec60cbae8770a616e62454297fd2abdc5b
Chris <morenochristopher580@gmail.com>
2026-09-14 17:29:49 -0500

    Update weapons.js

--- shared/weapons.js
+++ shared/weapons.js
@@ -1,6 +1,3 @@
-// Weapon definitions. Shared by server (damage, fire rate) and client
-// (viewmodel, zoom, recoil, reload animation timings, audio).
-
 export const WEAPONS = {
   rifle: {
     id: 'rifle',
@@ -23,6 +20,7 @@ export const WEAPONS = {
     viewmodel: 'rifle',
     range: 200,
     sound: 'gunshot_rifle',
+    projectile: false,
   },
 
   smg: {
@@ -46,12 +44,39 @@ export const WEAPONS = {
     viewmodel: 'smg',
     range: 120,
     sound: 'gunshot_smg',
+    projectile: false,
+  },
+
+  shotgun: {
+    id: 'shotgun',
+    name: 'Shotgun',
+    slot: 3,
+    damage: 12,                   // per pellet
+    headMult: 1.5,
+    pellets: 8,
+    spreadRad: 0.075,             // cone half-angle
+    fireRateMs: 800,
+    magSize: 6,
+    reloadMs: 2600,
+    auto: false,
+    adsZoom: 72,
+    adsTimeMs: 180,
+    recoilPitch: 0.045,
+    recoilYaw: 0.008,
+    recoilRecoverMs: 420,
+    vignetteStrength: 0.32,
+    hasScope: false,
+    moveMultAds: 0.6,
+    viewmodel: 'shotgun',
+    range: 35,
+    sound: 'gunshot_shotgun',
+    projectile: false,
   },
 
   sniper: {
     id: 'sniper',
     name: 'Sniper',
-    slot: 3,
+    slot: 4,
     damage: 90,
     headMult: 3,
     fireRateMs: 1400,
@@ -69,12 +94,13 @@ export const WEAPONS = {
     viewmodel: 'sniper',
     range: 400,
     sound: 'gunshot_sniper',
+    projectile: false,
   },
 
   pistol: {
     id: 'pistol',
     name: 'Pistol',
-    slot: 4,
+    slot: 5,
     damage: 30,
     headMult: 2,
     fireRateMs: 260,
@@ -92,13 +118,49 @@ export const WEAPONS = {
     viewmodel: 'pistol',
     range: 150,
     sound: 'gunshot_pistol',
+    projectile: false,
+  },
+
+  rpg: {
+    id: 'rpg',
+    name: 'RPG',
+    slot: 9,
+    adminOnly: true,
+    damage: 0,                    // direct hit is irrelevant, explosion does damage
+    headMult: 1,
+    fireRateMs: 2000,
+    magSize: 1,
+    reloadMs: 3500,
+    auto: false,
+    adsZoom: 70,
+    adsTimeMs: 200,
+    recoilPitch: 0.08,
+    recoilYaw: 0.01,
+    recoilRecoverMs: 600,
+    vignetteStrength: 0.4,
+    hasScope: false,
+    moveMultAds: 0.5,
+    viewmodel: 'rpg',
+    range: 400,
+    sound: 'gunshot_rpg',
+    projectile: true,
+
+    projectileSpeed: 30,          // units/sec
+    projectileGravity: 0,         // straight-line rocket
+    explosionRadius: 6,
+    explosionDamage: 120,         // at center, falls off to 0 at radius
+    selfDamageMult: 0.5,          // shooter takes half damage
   },
 };
 
 export const DEFAULT_WEAPON = 'rifle';
 
-export const WEAPON_ORDER = ['rifle', 'smg', 'sniper', 'pistol'];
+export const WEAPON_ORDER = ['rifle', 'smg', 'shotgun', 'sniper', 'pistol'];
 
 export function getWeapon(id) {
   return WEAPONS[id] || WEAPONS[DEFAULT_WEAPON];
+}
+
+export function isAdminWeapon(id) {
+  return !!WEAPONS[id]?.adminOnly;
 }
